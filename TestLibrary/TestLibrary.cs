798d302ac643330836f3791e3710bc00e2ef9efe
k.a <k.arashiro@iforcom.jp>
2014-06-30 14:39:10 +0900

    Initial commit

---
+++ TestLibrary/TestLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class Keywords
+    {
+        public string labelText
+        {
+            get { return "ライブラリ取得"; }
+        }
+    }
+
+    public static class StKeywords
+    {
+        public static string labelText
+        {
+            get { return "ライブラリ取得"; }
+        }
+    }
+}
